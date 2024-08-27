c2a13acfe641793acda97ecbc05da82946f04dd9
ilhaha <jyilhaha@163.com>
2024-08-27 18:36:39 +0800

    init

---
+++ yueyishou-parent/web/web-mgr/src/test/java/com/ilhaha/yueyishou/mgr/MallTinyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilhaha.yueyishou.mgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MallTinyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
