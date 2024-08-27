218f931e94540ee1c66cb01155dfc16fd6513aeb
ilhaha <jyilhaha@163.com>
2024-08-27 23:27:16 +0800

    后台管理系统加入微服务

---
+++ yueyishou-parent/server-gateway/src/main/java/com/ilhaha/yueyishou/ServiceGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.ilhaha.yueyishou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+public class ServiceGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceGatewayApplication.class);
+    }
+}
--- yueyishou-parent/web/web-mgr/src/main/java/com/ilhaha/yueyishou/ServiceWebMgrApplication.java
+++ yueyishou-parent/web/web-mgr/src/main/java/com/ilhaha/yueyishou/ServiceWebMgrApplication.java
@@ -3,13 +3,17 @@ package com.ilhaha.yueyishou;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @MapperScan("com.ilhaha.yueyishou.mgr.modules.ums.mapper")
-public class WebMgrApplication {
+@EnableDiscoveryClient
+@EnableFeignClients
+public class ServiceWebMgrApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebMgrApplication.class, args);
+        SpringApplication.run(ServiceWebMgrApplication.class, args);
     }
 
 }
